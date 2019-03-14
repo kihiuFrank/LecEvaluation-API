193123c3625c07fcebfdac599ff7f432bc4ef623
kihiuFrank <franklinekihiu@gmail.com>
2019-03-14 06:18:47 +0300

    first commit

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'update_user_info.php';
+$db = new update_user_info();
+ 
+// json response array
+$response = array("error" => FALSE);
+ 
+if (isset($_POST['reg_no']) && isset($_POST['password'])) {
+ 
+    // receiving the post params
+    $reg_no = $_POST['reg_no'];
+    $password = $_POST['password'];
+ 
+    // get the user by reg_no and password
+    $user = $db->VerifyUserAuthentication($reg_no, $password);
+ 
+    if ($user != false) {
+        // use is found
+        $response["error"] = FALSE;
+        $response["uid"] = $user["unique_id"];
+        $response["user"]["first_name"] = $user["first_name"];
+        $response["user"]["last_name"] = $user["last_name"];
+        $response["user"]["reg_no"] = $user["reg_no"];
+        $response["user"]["email"] = $user["email"];
+        $response["user"]["gender"] = $user["gender"];
+        echo json_encode($response);
+    } else {
+        // user is not found with the credentials
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Your login credentials are wrong. Please try again!";
+        echo json_encode($response);
+    }
+} else {
+    // required post params is missing
+    $response["error"] = TRUE; 
+    $response["error_msg"] = "Required parameters reg_no or password is missing!";
+    echo json_encode($response);
+}
+?>
---
+++ update_user_info.php
@@ -0,0 +1,148 @@
+<?php
+
+class update_user_info {
+
+    private $conn;
+
+    // constructor
+    function __construct() {
+        require_once 'android_login_connect.php';
+        // connecting to database
+        $db = new android_login_connect();
+        $this->conn = $db->connect();
+    }
+
+    // destructor
+    function __destruct() {
+
+    }
+
+    /**
+     * Storing new user
+     * returns user details
+     */
+    public function StoreUserInfo($first_name, $last_name, $reg_no, $email, $password, $gender) {
+        $hash = $this->hashFunction($password);
+        $encrypted_password = $hash["encrypted"]; // encrypted password
+        $salt = $hash["salt"]; // salt
+
+        $stmt = $this->conn->prepare("INSERT INTO android_php_post(first_name, last_name, reg_no, email, 
+        encrypted_password, salt, gender) VALUES(?, ?, ?, ?, ?, ?, ?)"); //? shows any datatype
+
+        $stmt->bind_param("sssssss", $first_name, $last_name, $reg_no, $email, $encrypted_password, $salt, $gender);
+        $result = $stmt->execute();
+        $stmt->close();
+
+        // check for successful store
+        if ($result) {
+            $stmt = $this->conn->prepare("SELECT first_name, last_name, reg_no, email, encrypted_password, salt, gender
+             FROM android_php_post WHERE reg_no = ?");
+            $stmt->bind_param("s", $reg_no);
+            $stmt->execute();
+            $stmt-> bind_result($token2,$token3,$token4,$token5,$token6,$token7,$token8);
+            while ( $stmt-> fetch() ) {
+               $user["first_name"] = $token2;
+               $user["last_name"] = $token3;
+               $user["reg_no"] = $token4;
+               $user["email"] = $token5;
+               $user["gender"] = $token8;
+            }
+            $stmt->close();
+            return $user;
+        } else {
+          return false;
+        }
+    }
+
+    /**
+     * Get user by registration number and password
+     */
+    public function VerifyUserAuthentication($reg_no, $password) {
+
+        $stmt = $this->conn->prepare("SELECT first_name, last_name, reg_no, email, encrypted_password, salt, gender
+             FROM android_php_post WHERE reg_no = ?");
+
+        $stmt->bind_param("s", $reg_no);
+
+        if ($stmt->execute()) {
+            $stmt-> bind_result($token2,$token3,$token4,$token5,$token6,$token7,$token8);
+
+            while ( $stmt-> fetch() ) {
+               $user["first_name"] = $token2;
+               $user["last_name"] = $token3;
+               $user["reg_no"] = $token4;
+               $user["email"] = $token5;
+               $user["encrypted_password"] = $token6;
+               $user["salt"] = $token7;
+               $user["gender"] = $token8;
+            }
+
+            $stmt->close();
+
+            // verifying user password
+            $salt = $token7;
+            $encrypted_password = $token6;
+            $hash = $this->CheckHashFunction($salt, $password);
+            // check for password equality
+            if ($encrypted_password == $hash) {
+                // user authentication details are correct
+                return $user;
+            }
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Check if user exists or not
+     */
+    public function CheckExistingUser($email, $reg_no) {
+        $stmt = $this->conn->prepare("SELECT email from android_php_post WHERE email = ?");
+
+        $stmt = $this->conn->prepare("SELECT reg_no from android_php_post WHERE reg_no = ?");
+
+        $stmt->bind_param("s", $email);
+        $stmt->bind_param("s", $reg_no);
+
+        $stmt->execute();
+
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            // user existed 
+            $stmt->close();
+            return true;
+        } else {
+            // user not existed
+            $stmt->close();
+            return false;
+        }
+    }
+
+    /**
+     * Encrypting password
+     * @param password
+     * returns salt and encrypted password
+     */
+    public function hashFunction($password) {
+
+        $salt = sha1(rand());
+        $salt = substr($salt, 0, 10);
+        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = array("salt" => $salt, "encrypted" => $encrypted);
+        return $hash;
+    }
+
+    /**
+     * Decrypting password
+     * @param salt, password
+     * returns hash string
+     */
+    public function checkHashFunction($salt, $password) {
+        $hash = base64_encode(sha1($password . $salt, true) . $salt);
+        return $hash;
+    }
+
+}
+
+?>
