41ede826b745103470fa4b8505cdcb30f77994f5
kihiuFrank <franklinekihiu@gmail.com>
2019-03-17 14:34:22 +0300

    Commit; error fix

--- android_login_config.php
+++ android_login_config.php
@@ -2,11 +2,7 @@
   
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-<<<<<<< HEAD
 define("DB_PASSWORD", "FrankKihiu");
-=======
-define("DB_PASSWORD", "");
->>>>>>> 193123c3625c07fcebfdac599ff7f432bc4ef623
 define("DB_DATABASE", "LecEvaluation");
  
 ?>
